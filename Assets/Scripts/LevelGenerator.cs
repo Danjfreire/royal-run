aea2fba3582311c13e8afa61fef0cc8ccd9a5c08
Daniel <danjfreire@gmail.com>
2025-01-02 13:55:55 -0300

    feat: moving level chunks

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -7,20 +7,42 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private Transform levelChunkParent;
     [SerializeField] private int chunkAmount;
     [SerializeField] private float chunkLength;
+    [SerializeField] private float chunkMoveSpeed;
+
+    private GameObject[] levelChunks;
+
+    private void Awake()
+    {
+        levelChunks = new GameObject[chunkAmount];
+    }
 
     private void Start()
     {
         GenerateLevel();
     }
 
+    private void Update()
+    {
+        MoveChunks();
+    }
+
     private void GenerateLevel()
     {
-
         for (int i = 0; i < chunkAmount; i++)
         {
             float zOffset = i * chunkLength;
             Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z + zOffset);
-            Instantiate(levelChunkPrefab, position, Quaternion.identity, levelChunkParent);
+            GameObject levelChunk = Instantiate(levelChunkPrefab, position, Quaternion.identity, levelChunkParent);
+            levelChunks[i] = levelChunk;
+        }
+    }
+
+    private void MoveChunks()
+    {
+        float zOffset = Time.deltaTime * chunkMoveSpeed;
+        for (int i = 0; i < levelChunks.Length; i++)
+        {
+            levelChunks[i].transform.Translate(-transform.forward * zOffset);
         }
     }
 }
