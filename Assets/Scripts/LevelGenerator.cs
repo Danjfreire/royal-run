9db4d14ccc53c05d8f9509db6bfb38a99e55fdf3
Daniel <danjfreire@gmail.com>
2025-01-03 10:28:39 -0300

    feat: endless spawning of level chunks

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
@@ -9,16 +10,11 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private float chunkLength;
     [SerializeField] private float chunkMoveSpeed;
 
-    private GameObject[] levelChunks;
-
-    private void Awake()
-    {
-        levelChunks = new GameObject[chunkAmount];
-    }
+    private List<GameObject> levelChunks = new List<GameObject>();
 
     private void Start()
     {
-        GenerateLevel();
+        GenerateLevelChunks();
     }
 
     private void Update()
@@ -26,23 +22,49 @@ public class LevelGenerator : MonoBehaviour
         MoveChunks();
     }
 
-    private void GenerateLevel()
+    private void GenerateLevelChunks()
     {
         for (int i = 0; i < chunkAmount; i++)
         {
-            float zOffset = i * chunkLength;
-            Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z + zOffset);
-            GameObject levelChunk = Instantiate(levelChunkPrefab, position, Quaternion.identity, levelChunkParent);
-            levelChunks[i] = levelChunk;
+            SpawnChunk();
         }
     }
 
     private void MoveChunks()
     {
         float zOffset = Time.deltaTime * chunkMoveSpeed;
-        for (int i = 0; i < levelChunks.Length; i++)
+        for (int i = 0; i < levelChunks.Count; i++)
         {
-            levelChunks[i].transform.Translate(-transform.forward * zOffset);
+            GameObject chunk = levelChunks[i];
+            chunk.transform.Translate(-transform.forward * zOffset);
+
+            // if chunk gets out of view, remove it and spawn a new one as far as possible
+            if (chunk.transform.position.z <= Camera.main.transform.position.z - chunkLength)
+            {
+                levelChunks.Remove(chunk);
+                Destroy(chunk);
+                SpawnChunk();
+            }
         }
+    }
+
+    private void SpawnChunk()
+    {
+        float spawnPositionZ;
+
+        if (levelChunks.Count == 0)
+        {
+            // if there are no level chunks yet, spawn a new one at the parent origin
+            spawnPositionZ = levelChunkParent.transform.position.z;
+        }
+        else
+        {
+            // otherwise, get the last chunk and spawn next to it
+            spawnPositionZ = levelChunks[levelChunks.Count - 1].transform.position.z + chunkLength;
+        }
+
+        Vector3 position = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
+        GameObject levelChunk = Instantiate(levelChunkPrefab, position, Quaternion.identity, levelChunkParent);
+        levelChunks.Add(levelChunk);
     }
 }
