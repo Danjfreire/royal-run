ebe35e70a1f1d1756cb9e3fc86a8a1509dde4c7f
Daniel <danjfreire@gmail.com>
2025-01-04 09:16:09 -0300

    spawning multiple fences

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Chunk : MonoBehaviour
@@ -7,8 +8,23 @@ public class Chunk : MonoBehaviour
 
     private void Start()
     {
-        int laneIndex = Random.Range(0, lanePositions.Length);
-        Vector3 spawnPosition = new Vector3(lanePositions[laneIndex], transform.position.y, transform.position.z);
-        Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
+        SpawnFences();
+    }
+
+    private void SpawnFences()
+    {
+        List<int> availableLanes = new List<int> { 0, 1, 2 };
+        int fencesToSpawn = Random.Range(0, availableLanes.Count);
+
+        for (int i = 0; i < fencesToSpawn; i++)
+        {
+            if (availableLanes.Count == 0) break;
+
+            int laneIndex = Random.Range(0, availableLanes.Count);
+            Vector3 spawnPosition = new Vector3(lanePositions[laneIndex], transform.position.y, transform.position.z);
+            Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
+            availableLanes.RemoveAt(laneIndex);
+        }
+
     }
 }
