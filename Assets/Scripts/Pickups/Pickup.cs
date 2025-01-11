cdeaf217211e5696b7ff42421ed227d1d55bab02
Daniel <danjfreire@gmail.com>
2025-01-11 10:08:26 -0300

    feat: start implementing pickup logic

--- Assets/Scripts/Pickups/Pickup.cs
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
 
-public class Pickup : MonoBehaviour
+public abstract class Pickup : MonoBehaviour
 {
+    [SerializeField] private float rotationSpeed = 100f;
+
+    private void Update()
+    {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<PlayerController>() != null)
         {
-            Debug.Log("Picked up by:" + other.gameObject.name);
+            OnPickUp();
+            Destroy(gameObject);
         }
     }
+
+    protected abstract void OnPickUp();
 }
