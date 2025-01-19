47c8ec5400bf2fd2c784c8d459ce64cbe6b40bef
Daniel Freire <danjfreire@gmail.com>
2025-01-19 11:17:45 -0300

    feat: add checkpoint skin

--- Assets/Scripts/Proc Gen/Checkpoint.cs
+++ Assets/Scripts/Proc Gen/Checkpoint.cs
@@ -1,8 +1,15 @@
+using TMPro;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
     [SerializeField] private float increaseTime = 5f;
+    [SerializeField] private TMP_Text checkPointText;
+
+    private void Start()
+    {
+        checkPointText.text = "+" + increaseTime.ToString();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
