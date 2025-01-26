16a05ce605975b7153397e20709265a7d229f9e3
Daniel <danjfreire@gmail.com>
2025-01-26 11:19:20 -0300

    feat: add rock collision fx

--- Assets/Scripts/Obstacles/Rock.cs
+++ Assets/Scripts/Obstacles/Rock.cs
@@ -5,20 +5,50 @@ using System;
 public class Rock : MonoBehaviour
 {
     [SerializeField] float shakeMultiplier = 10f;
+    [SerializeField] ParticleSystem collisionParticles;
+    [SerializeField] AudioSource rockCollisionSound;
+    [SerializeField] float collisionFXcooldown = 1f;
 
     CinemachineImpulseSource inpulseSource;
+    float currentFxCooldown = 0f;
 
     private void Awake()
     {
         inpulseSource = GetComponent<CinemachineImpulseSource>();
     }
 
+    private void Update()
+    {
+        currentFxCooldown += Time.deltaTime;
+    }
+
     private void OnCollisionEnter(Collision other)
+    {
+        ShakeScreen();
+        PlayCollisionFX(other);
+    }
+
+    private void ShakeScreen()
     {
         float distance = Vector3.Distance(transform.position, Camera.main.transform.position);
         float shakeIntensity = (1f / distance) * shakeMultiplier;
         shakeIntensity = Math.Min(shakeIntensity, 0.8f);
 
         inpulseSource.GenerateImpulse(shakeIntensity);
+    }
+
+    private void PlayCollisionFX(Collision other)
+    {
+        if (currentFxCooldown < collisionFXcooldown)
+        {
+            return;
+        }
+
+        ContactPoint contactPoint = other.contacts[0];
+        collisionParticles.transform.position = contactPoint.point;
+        collisionParticles.Play();
+        rockCollisionSound.Play();
+
+        currentFxCooldown = 0;
     }
 }
